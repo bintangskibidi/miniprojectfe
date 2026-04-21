2ced795315891e3ebc95344bd3589bbebf54482c
nnbieelll <NabilUlhaqSatria.TKJ1.24@binusasmg.sch.id>
2026-04-21 16:18:43 +0700

    data siswa

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import DataWaliKelas from "./dashboard/manajemenSiswa/kesiswaan/DataWaliKelas";
 import ManajemenGuru from "./dashboard/manajemenGuru/ManajemenGuru";
 import ManajemenSiswa from "./dashboard/manajemenSiswa/ManajemenSiswa";
 import ManajemenKeuangan from "./dashboard/manajemenKeuangan/ManajemenKeuangan";
- 
-   
 import ManajemenPerputakaan from "./dashboard/perpustakaanDigital/ManajemenPerpustkaan";
 import MataPelajaran from "./dashboard/manajemenGuru/MataPelajaran";
 import SettingLokasi from "./dashboard/manajemenSekolah/SettingLokasi";
@@ -19,16 +17,11 @@ import Semester from "./dashboard/manajemenSiswa/e-raport/Semester";
 import Tambahsiswa from "./dashboard/manajemenSiswa/kesiswaan/dataSiswa/AddDataSiswa";
 import EditSiswa from "./dashboard/manajemenSiswa/kesiswaan/dataSiswa/EditDataSiswa";
 import Extrakulikuler from "./dashboard/manajemenSiswa/kegiatan/extrakulikuler";
- 
 import Layout from "./component/sideBar/Layout";
 import "./App.css";
 import JadwalMengajar from "./dashboard/manajemenGuru/JadwalMengajar";
- import DataBuku from "./dashboard/perpustakaanDigital/DataBuku";
- 
 import DataJurusan from "./dashboard/manajemenSiswa/kesiswaan/DataJurusan";
 import KenaikanKelas from "./dashboard/manajemenSiswa/kesiswaan/KenaikanKelas";
- 
-
 
 function App() {
   return (
@@ -36,31 +29,32 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-
           <Route path="datasiswa" element={<DataSiswa />} />
-          <Route path="databuku" element={<DataBuku />} />
           <Route path="walikelas" element={<DataWaliKelas />} />
-          <Route path="semester" element={<Semester />} />
-
-          <Route path="tambah-siswa" element={<Tambahsiswa />} />
-          <Route path="ekstrakurikuler" element={<Extrakulikuler />} />
+          <Route path="Semester" element={<Semester />} />
+          <Route
+            path="/dashboard/ekstrakurikuler"
+            element={<Extrakulikuler />}
+          />
 
           <Route path="siswa" element={<ManajemenSiswa />} />
           <Route path="guru" element={<ManajemenGuru />} />
-          <Route path="mapel" element={<MataPelajaran />} />
-          <Route path="jadwalmengajar" element={<JadwalMengajar />} />
-
-          <Route path="datajurusan" element={<DataJurusan />} />
-          <Route path="kenaikankelas" element={<KenaikanKelas />} />
-<Route path="edit-siswa/:id" element={<EditSiswa />} />
+          <Route path="/dashboard/mapel" element={<MataPelajaran />} />
+          <Route
+            path="/dashboard/JadwalMengajar"
+            element={<JadwalMengajar />}
+          />
+          <Route path="/dashboard/datajurusan" element={<DataJurusan />} />
+          <Route path="/dashboard/tambah-siswa" element={<Tambahsiswa />} />
+          <Route path="/dashboard/edit-siswa/:id" element={<EditSiswa />} />
+          <Route path="/dashboard/kenaikankelas" element={<KenaikanKelas />} />
           <Route path="keuangan" element={<ManajemenKeuangan />} />
           <Route path="perpustakaan" element={<ManajemenPerputakaan />} />
-          <Route path="profilesekolah" element={<ProfileSekolah />} />
+          <Route path="profileSekolah" element={<ProfileSekolah />} />
           <Route path="settinglokasi" element={<SettingLokasi />} />
-
           <Route path="datakelas" element={<DataKelas />} />
           <Route path="tahunajaran" element={<TahunAjaran />} />
         </Route>
@@ -68,3 +62,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
+export default App;
